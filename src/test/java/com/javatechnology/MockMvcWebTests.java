2ed27ab249bd439ac524a009dc20adf69e89bce6
Suresh <Suresh@192.168.0.3>
2021-07-17 14:55:52 +0530

    added zip file

---
+++ src/test/java/com/javatechnology/MockMvcWebTests.java
@@ -0,0 +1,5 @@
+package com.javatechnology;
+
+public class MockMvcWebTests {
+
+}
--- src/test/java/com/javatechnology/ReadinglistApplicationTests.java
+++ src/test/java/com/javatechnology/ReadinglistApplicationTests.java
@@ -1,14 +1,42 @@
 package com.javatechnology;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.Matchers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class ReadinglistApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+
+	}
+
+	@Autowired
+	private WebApplicationContext webContext;
+	//@Autowired
+	private MockMvc mockMvc;
+
+	// @Before
+	public void setupMockMvc() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void homePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/reader"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("readingList"))
+				.andExpect(MockMvcResultMatchers.model().attributeExists("books"));
+				//.andExpect(MockMvcResultMatchers.model().attribute("books", Matchers.is(Matchers.empty())));
 	}
 
 }
